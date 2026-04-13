5251bb209bff39acd4352c4f431ff659e3e090bb
Laxmi Narayan <laxmi.narayan.main1@gmail.com>
2026-04-13 14:47:44 +0530

    fixes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 // const API_URL = "http://localhost:8000/api";
-const API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+const BASE_API_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
+
+const API_URL = BASE_API_URL + "/api";
 
 const api = axios.create({
   baseURL: API_URL,
